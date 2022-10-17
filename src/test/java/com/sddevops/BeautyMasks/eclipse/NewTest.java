34e0f40c9886b57d0c7d546b0b6e5573ff1e2dd0
chai-ru-yi <22053379@myrp.edu.sg>
2022-10-17 12:24:46 -0700

    Added files

---
+++ src/test/java/com/sddevops/BeautyMasks/eclipse/NewTest.java
@@ -0,0 +1,73 @@
+package com.sddevops.BeautyMasks.eclipse;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+public class NewTest {
+	// declare Selenium WebDriver
+	private WebDriver webDriver;
+	
+	
+
+
+	@Test
+	public void checkTitle1() {
+		webDriver.navigate().to("http://localhost:8090/BeautyMasks/ProductServlet/dashboard");
+
+		Assert.assertEquals(webDriver.getTitle(), "Product List");
+
+		System.out.println("Title: " + webDriver.getTitle());
+
+
+		webDriver.findElement(By.className("btn-success")).click();
+
+		Assert.assertTrue(webDriver.getTitle().contains("Add Product Listing"));
+		System.out.println("New title: " + webDriver.getTitle());
+	}
+
+	@Test
+	public void checkTitle2() {
+		webDriver.navigate().to("http://localhost:8090/BeautyMasks/AddProductListing.jsp");
+
+		Assert.assertEquals(webDriver.getTitle(), "Add Product Listing");
+
+		System.out.println("Title: " + webDriver.getTitle());
+
+		webDriver.findElement(By.className("btn-success")).click();
+
+		
+		System.out.println("New title: " + webDriver.getTitle());
+	}
+
+
+
+	@BeforeTest
+	public void beforeTest() {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\Google\\Chrome\\chromedriver.exe");
+
+		webDriver = new ChromeDriver();
+	}
+
+
+
+	@AfterTest
+	public void afterTest() {
+		
+		// webDriver.quit();
+	}
+	
+	//chai ru yi
+
+}
